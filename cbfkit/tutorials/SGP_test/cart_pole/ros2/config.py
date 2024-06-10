7f31cc7b57a45335c5e4a7a73c963fadb4410caf
ShashwatPatil <shashwatpatil974@gmail.com>
2024-06-10 16:23:37 +0530

    modified the genertae model  to fix params in config.py

--- cbfkit/tutorials/SGP_test/cart_pole/ros2/config.py
+++ cbfkit/tutorials/SGP_test/cart_pole/ros2/config.py
@@ -9,14 +9,14 @@ MODULE_NAME = "cart_pole"
 # Controller
 CONTROLLER_NAME = "controller_1"
 TIMER_INTERVAL = 0.1
-CONTROLLER_PARAMS = {control_args}
+CONTROLLER_PARAMS = k_1 : float, k_2 : float, k_3 : float, k_4 : float, G : float, l : float, 
 
 # Estimator
 ESTIMATOR_NAME = "naive"
 
 # Plant
 PLANT_NAME = "plant"
-PLANT_PARAMS = {dynamics_args}
+PLANT_PARAMS = I : float, M : float, m : float, G : float, l : float, 
 INTEGRATOR_NAME = "forward_euler"
 DT = 0.01
 
